c5a9c8324603cdff0166380442cf737cbd7a8f30
andrey2503 <andreytorresvega@gmail.com>
2018-09-02 11:03:48 -0600

    controles nave

---
+++ Assets/Scripts/player/player_engine.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_engine : MonoBehaviour {
+	public GameObject player;
+	public GameObject up;
+	public GameObject down;
+	public static player_engine instance;
+	public float mover=0.50f;
+	public float velocidadRotacion=0.05f;
+	// Use this for initialization
+	void Awake(){
+		if(player_engine.instance == null){
+			player_engine.instance = this;
+		}else{
+			Destroy (this.gameObject);
+		}// else
+	}// fin del Awake
+	void Start () {
+		
+	}
+
+	public float getVelocityPostY (){
+		return  player.GetComponent<Rigidbody2D> ().velocity.y;
+	}
+
+	public void moverUP(){
+		//player.GetComponent<Rigidbody2D> ().velocity = new Vector2(mover,getVelocityPostY());
+		//var heading = up.transform.position - down.transform.position;
+		//var distance = heading.magnitude;
+		//var direction = heading / distance;
+		Debug.Log ("mover up");
+		//player.transform.position = up.transform.position;
+		player.transform.position = Vector3.Lerp(player.transform.position, up.transform.position, mover);
+	}// FIN DE MOVERuP
+
+	public void direccion(){
+		var heading = up.transform.position - down.transform.position;
+		var distance = heading.magnitude;
+		var direction = heading / distance;
+		Debug.Log (direction);
+	}
+
+	public void girarDerecha(){
+		player.transform.Rotate(Vector3.right * Time.deltaTime * velocidadRotacion);
+	}// fin de girar derecha
+
+	public void girarIzquierda(){
+		player.transform.Rotate(Vector3.left * Time.deltaTime * velocidadRotacion);
+	}
+
+}
---
+++ Assets/Scripts/player/player_inputs.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_inputs : MonoBehaviour {
+	
+	public static player_inputs instance;
+	void Awake(){
+		if(player_inputs.instance == null){
+			player_inputs.instance = this;
+		}else{
+			Destroy (this.gameObject);
+		}// else
+	}// fin del Awake
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void FixedUpdate () {
+		float moverUp = Input.GetAxis ("Vertical");
+		if(moverUp > 0){
+			Debug.Log (moverUp);
+			Debug.Log ("se esta presionado arriba");
+			player_engine.instance.moverUP ();
+
+		}else if(moverUp < 0){
+			
+		}
+
+
+		float mover = Input.GetAxis ("Horizontal");
+		if (mover > 0) {
+			player_engine.instance.girarDerecha ();
+			//mover a la derecha
+		} else if (mover < 0) {
+			player_engine.instance.girarIzquierda ();
+			//mover a la izquierda
+		}// fin de if izquierda Derecha
+
+	}// fin de fixedupdate
+}
