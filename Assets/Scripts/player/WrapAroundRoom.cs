7d6e933994633e32d2e6865dc1d53b98ff5d18b2
andrey2503 <andreytorresvega@gmail.com>
2018-09-12 10:42:11 -0600

    movimiento nave

---
+++ Assets/Scripts/player/WrapAroundRoom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrapAroundRoom : MonoBehaviour {
+
+	public float limiteHorizontal;
+	public float limiteVertical;
+	// Use this for initialization
+	void Start () {
+		//limiteHorizontal = 13.32f;
+		//limiteVertical = 9.54f;
+		limiteHorizontal= Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,0,transform.position.z- Camera.main.transform.position.z)).x;
+		limiteVertical = Camera.main.ScreenToWorldPoint(new Vector3(0,Screen.height,transform.position.z- Camera.main.transform.position.z)).y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x	> limiteHorizontal){
+			transform.position = new Vector3 (-limiteHorizontal,transform.position.y,0);
+		}
+		if(transform.position.x	< -limiteHorizontal){
+			transform.position = new Vector3 (limiteHorizontal,transform.position.y,0);
+		}
+		if(transform.position.y	> limiteVertical){
+			transform.position = new Vector3 (transform.position.x,-limiteVertical,0);
+		}
+		if(transform.position.y	< -limiteVertical){
+			transform.position = new Vector3 (transform.position.x,limiteVertical,0);
+		}
+	}// fin del update
+}
---
+++ Assets/Scripts/player/tutorial_movimiento.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorial_movimiento : MonoBehaviour {
+
+	public GameObject player;
+	public float velocidadMovimiento=0f;
+	public float velocidadRotacion=50f;
+	public Vector3 nuevaDireccion;
+	Rigidbody2D mirigidbody;
+	// Use this for initialization
+	void Start () {
+		nuevaDireccion = transform.right;
+		mirigidbody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.forward,- velocidadRotacion * Input.GetAxis("Horizontal")* Time.deltaTime);
+
+		// movimiento kinematico, no afectado con fisica
+		/*
+		if(Input.GetKey(KeyCode.UpArrow)){
+			velocidadMovimiento += 0.1f;
+			nuevaDireccion = transform.right;
+		}
+		velocidadMovimiento = Mathf.Clamp (velocidadMovimiento,0,10);
+		player.transform.Translate (nuevaDireccion*velocidadMovimiento*Time.deltaTime,Space.World);
+		*/
+	}// fin del update
+
+	void FixedUpdate(){
+		// movimiento no kinematico, afectado por fisicas
+		velocidadMovimiento = Mathf.Clamp (velocidadMovimiento,0,10);
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			mirigidbody.AddForce (transform.up * velocidadMovimiento);
+		}// fin del if
+		velocidadMovimiento = Mathf.Clamp (velocidadMovimiento,0,5);
+	}// fin de fixdUpdate
+}
